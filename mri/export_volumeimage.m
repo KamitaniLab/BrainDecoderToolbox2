fd59c1c23b0f20e12fb8875b9a8fbfd4b1854eb3
Shuntaro Aoki <aoki@atr.jp>
2017-07-21 14:41:04 +0900

    mri: add export_volumeimage.m

---
+++ mri/export_volumeimage.m
@@ -0,0 +1,40 @@
+function export_volumeimage(filename, dat, xyz, template)
+% export_volumeimage    Exports a brain image
+%
+% This file is a part of BrainDecoderToolbox2.
+%
+% Inputs:
+%
+% - filename [char]       : Path to the exported brain image file
+% - dat      [N-d vector] : Voxel data
+% - xyz      [N-d vector] : Voxel xyz
+% - template [char]       : Path to a template (space defining) image file
+%
+% Outputs:
+%
+% None
+%
+
+verbose = true;
+missval = NaN;
+
+if verbose, fprintf('Loading %s\n', template); end
+tempvol = spm_vol(template);
+[tempdat, tempxyz] = spm_read_vols(tempvol);
+
+[dupxyz, inddat, indtemp] = intersect(xyz', tempxyz', 'rows');
+
+outvol.fname   = filename;
+outvol.dim     = tempvol.dim;
+outvol.dt      = tempvol.dt;
+outvol.mat     = tempvol.mat;
+outvol.n       = tempvol.n;
+outvol.descrip = tempvol.descrip;
+outvol.private = tempvol.private;
+
+outdat = tempdat;
+outdat(:) = missval;
+outdat(indtemp) = dat(inddat);
+
+if verbose, fprintf('Writing %s\n', outvol.fname); end
+spm_write_vol(outvol, outdat);
